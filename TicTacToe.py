c87e41e911a1ad247ecbebec7f32f240f0a84ab5
PRINCE ADVANCE <princeadvanceagbenu@gmail.com>
2022-05-26 05:56:25 +0100

    first commit

---
+++ TicTacToe.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 23 08:21:47 2021
+
+@author: Advance
+"""
+
+theBoard = {'top-L':'','top-M':'','top-R':'',
+             'mid-L':'','mid-M':'','mid-R':'',
+             'low-L':'','low-M':'','low-R':''}
+def printBoard(board):
+    print(board['top-L'] + ' | ' + board['top-M'] + '| ' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + ' | ' + board['mid-M'] + '| ' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + ' | ' + board['low-M'] + '| ' + board['low-R'])
+
+
+turn = 'X'
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '.Move to the other space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'x':
+        turn = '0'
+    else:
+        turn = 'X'
+printBoard(theBoard)
